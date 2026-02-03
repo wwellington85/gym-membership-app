d27bfa37ed843ffa15455c9cae994b4610b37362
Winfield Wellington <winfield.wellington@gmail.com>
2026-02-03 14:28:38 -0500

    Suppress hydration warnings on body

--- app/layout.tsx
+++ app/layout.tsx
@@ -26,7 +26,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" suppressHydrationWarning>
-      <body className={`${geistSans.className} antialiased`}>
+      <body suppressHydrationWarning className={`${geistSans.className} antialiased`}>
         <ThemeProvider
           attribute="class"
           defaultTheme="system"
